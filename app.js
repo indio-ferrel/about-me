bdd0f3a2a4e4cff7745730cc1d1b88ccf882b603
Indio Ferrel <mferrel1911@gmail.com>
2022-04-20 14:48:50 -0700

    Added (possibly complete) plan to readme.md

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 // import functions
+const button = document.getElementById('button');
+const nameEl = document.getElementById('name');
+const sectionEl = document.getElementById('my-info');
+const animalButton = document.getElementById('animal-button');
+const animalDiv = document.getElementById('animal-div');
 
+// console.log(animalDiv.classList);
 // grab DOM elements
 
 // set event listeners 
+button.addEventListener('click', () => {
+    if (nameEl.textContent === '___') {
+        nameEl.textContent = 'Indio';
+    } else {
+        nameEl.textContent = '___';
+    }
+    sectionEl.classList.toggle('my-style');
+});
+
+animalButton.addEventListener('click', () => {
+    animalDiv.classList.remove('hidden');
+});
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
